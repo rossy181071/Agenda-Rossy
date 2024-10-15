54ada393d09f73c82a94182d0afe3bd95ec9134f
Rossy Lopez <rossy181071@hotmail.com>
2024-10-15 17:20:26 -0600

    Created Navbar-component

---
+++ src/navbar-component.js
@@ -0,0 +1,69 @@
+import  { LitElement, html, css } from "lit";
+
+export class NavbarComponent extends LitElement { 
+    static get properties() {
+        return {
+            pages: { type: Array }
+        }
+
+    }
+    constructor () {
+        super()
+        this.pages = [
+        {
+            url: 'homepage',
+            title:'Agencia Seminuevos'
+        },
+        {
+            url: 'bio',
+            title: 'Autos Sedan'
+        },
+        {
+            url: 'lit',
+            title: 'Pick Up'
+        },
+        {
+            url: 'react',
+            title: 'Camiones'
+        }
+        
+        ]
+    }
+
+    static get styles() {
+        return css`
+        #navbar-container {
+            background-color: #225897;
+
+            display: table;
+            width: 100%;
+            table-layout: fixed;
+        }
+        
+        #navbar-container h2 {
+            display: table-cell;
+            text-align: center;
+
+            background-color: #f35454;
+            padding: 20 px;
+        }
+       
+       `
+
+    }
+
+    triggerPageChange(e) {
+        this.dispatchEvent(new CustomEvent('change-page', {
+            detail: e.target.id
+        }))
+    }
+    render() {
+        return html`
+        <div id="navbar-container">
+        ${this.pages.map(page => html`<h2 id="${page.url}" @click="${this.triggerPageChange}">${page.title}</h2>`)}
+        </div>
+        `
+    }
+}
+
+window.customElements.define('navbar-component', NavbarComponent)
